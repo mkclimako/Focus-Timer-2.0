ec8b9a694b6133826cedcbbb5a3423e81fd72cc3
markoclimako <mkclimako@gmail.com>
2022-05-15 03:55:36 -0300

    global variables created, modularization| In progress

---
+++ scripts/elements.js
@@ -0,0 +1,24 @@
+const body = document.querySelector('body');
+
+const playButton = document.querySelector('.play');
+const stopButton = document.querySelector('.stop');
+
+const forestSound = document.querySelector('.forest');
+const rainSound = document.querySelector('.rain');
+const fireplaceSound = document.querySelector('.fireplace');
+const coffeShopSound = document.querySelector('.coffeShop');
+
+const moreMinutesButton = document.querySelector('.moreMinutes');
+const lessMinutesButton = document.querySelector('.lessMinutes');
+
+export {
+  playButton,
+  stopButton,
+  forestSound,
+  rainSound,
+  fireplaceSound,
+  coffeShopSound,
+  moreMinutesButton,
+  lessMinutesButton,
+  body
+};
