5d8887c4552e524c15f91302d5f26c4e7d620a11
markoclimako <mkclimako@gmail.com>
2022-05-16 21:31:48 -0300

    Sound control feature| implemented

--- scripts/sounds.js
+++ scripts/sounds.js
@@ -1,6 +1,31 @@
+export default function Sounds() {
+  const clickedSound = new Audio('./src/sounds/clickSound.mp3');
+  const timeEnd = new Audio('./src/sounds/timeEnd.mp3');
 
-    export const forest = new Audio('./src/sounds/Floresta.wav');
-    export const rain = new Audio('./src/sounds/Chuva.wav');
-    export const fireplace = new Audio('./src/sounds/Lareira.wav');
-    export const coffeeShop = new Audio('./src/sounds/Cafeteria.wav');
+  const forest = new Audio('./src/sounds/Floresta.wav');
+  const rain = new Audio('./src/sounds/Chuva.wav');
+  const fireplace = new Audio('./src/sounds/Lareira.wav');
+  const coffeeShop = new Audio('./src/sounds/Cafeteria.wav');
 
+  forest.loop = true;
+  rain.loop = true;
+  fireplace.loop = true;
+  coffeeShop.loop = true;
+
+  const buttonClickSound = () => {
+    clickedSound.play();
+  };
+
+  const sessionCompleted = () => {
+    timeEnd.play();
+  };
+
+  return {
+    buttonClickSound,
+    sessionCompleted,
+    forest,
+    rain,
+    fireplace,
+    coffeeShop,
+  };
+}
